e247e80934a302aa67f7641d86058417905dd7aa
jj200031 <jaxeverh@gmail.com>
2023-03-10 22:47:45 -0500

    Initial commit

---
+++ src/main/java/gamblev/jaxson/gvutils/facutils.java
@@ -0,0 +1,14 @@
+package gamblev.jaxson.gvutils;
+import org.bukkit.plugin.java.JavaPlugin;
+public class facutils extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        
+    }
+    @Override
+    public void onDisable() {
+
+    }
+
+
+}
